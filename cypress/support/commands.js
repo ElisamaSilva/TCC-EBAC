1ce7ee30f4f807b2f738b30440fd56d4acae0cd9
Elisama Alves Nascimento Da Silva <ealvesns@gmail.com>
2023-09-09 16:47:42 -0300

    Adicionando script do teste web

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,9 @@
-/*Cypress.Commands.add('addProdutos', (produto, quantidade) => {
+Cypress.Commands.add('addProduto', (produto, quantidade) => {
     cy.get('[class= "product-block grid"]')
-        .contains(produto).click()
+      .contains(produto).click()
     cy.get('.button-variable-item-M').click()
     cy.get('.button-variable-item-Blue').click()
     cy.get('.input-text').clear().type(quantidade)
     cy.get('.single_add_to_cart_button').click()
-})*/
+
+})
