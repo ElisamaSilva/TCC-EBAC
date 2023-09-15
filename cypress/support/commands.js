32763178afcc49dacc49ba47d1ab56b868cbfe8e
Elisama Alves Nascimento Da Silva <ealvesns@gmail.com>
2023-09-14 23:54:19 -0300

    Teste API

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,13 @@
-Cypress.Commands.add('addProduto', (produto, quantidade) => {
-    cy.get('[class= "product-block grid"]')
-      .contains(produto).click()
-    cy.get('.button-variable-item-M').click()
-    cy.get('.button-variable-item-Blue').click()
-    cy.get('.input-text').clear().type(quantidade)
-    cy.get('.single_add_to_cart_button').click()
-
+Cypress.Commands.add('autenticacao', (usuario, senha) => {
+  cy.request({
+      method: 'POST',
+      url: "http://lojaebac.ebaconline.art.br/rest-api/auth",
+      body: {
+          "usuario": usuario,
+          "senha": senha 
+      }
+  }).then((response) => {
+      expect(response.status).to.equal(200)
+      return response.body.authorization
+  })
 })
